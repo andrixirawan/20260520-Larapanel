8f874a2ca7ca84b3d41e5236592a2f326ccffc4a
Andri Irawan <andrixirawan@gmail.com>
2026-06-12 00:07:05 +0700

    Update asset filenames in manifest.json for improved clarity and consistency

--- public/build/assets/dist-B0-mcD5B.js
+++
@@ -1,1 +0,0 @@
-import{At as e,Mt as t}from"./app-CWxAtIoU.js";var n=t(e(),1);function r(e){let t=n.useRef({value:e,previous:e});return n.useMemo(()=>(t.current.value!==e&&(t.current.previous=t.current.value,t.current.value=e),t.current.previous),[e])}export{r as t};
