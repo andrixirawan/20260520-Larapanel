eecb5f089661c6591ec7af5f0f128ccd167855e2
Andri Irawan <andrixirawan@gmail.com>
2026-06-15 22:20:27 +0700

    build last commit

---
+++ public/build/assets/check-BSRzwtyw.js
@@ -0,0 +1,1 @@
+import{xt as e}from"./app-BZu8_tR4.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
--- public/build/assets/check-C-m7lBV3.js
+++
@@ -1,1 +0,0 @@
-import{xt as e}from"./app-_HdZP79D.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
